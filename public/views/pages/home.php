45868c5eb31c9a2eb23290cb2ba51812fa221489
Etudiant <info@bdac.eu>
2019-07-03 14:19:53 +0200

    Refactor

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -1,28 +1,18 @@
 <?php ob_start(); ?>
 
-<h1>Bienvenue chez Petit Génie !</h1>
+<h1>Bienvenue !</h1>
 
-<div class="col-4">
-    <a href="listeBiens.php" class="btn btn-sm btn-secondary">
-        Liste des produits
-    </a>
-</div>
+<hr>
 
-<div class="col-4">
-    <a href="listeBiens.php" class="btn btn-sm btn-secondary">
-        Liste des clients
-    </a>
-</div>
-<div class="col-4">
-    <a href="listeBiens.php" class="btn btn-sm btn-secondary">
-        Ajout d'un produit
-    </a>
-</div>
-<div class="col-4">
-    <a href="listeBiens.php" class="btn btn-sm btn-secondary">
-        Ajout d'un client
-    </a>
-</div>
+<ul>
+    <li>Liste des produits</li>
+    <li>Liste des clients</li>
+</ul>
+
+<ul>
+    <li>Ajouter un produit</li>
+    <li>Ajouter un client</li>
+</ul>
 
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
