3a3fd74b9232cb7476c8c47a6d2e3b98dfe1a347
Etudiant <info@bdac.eu>
2019-07-03 14:28:27 +0200

    modify links

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -6,19 +6,19 @@
 
 <ul>
     <li>
-        <a href="listeBiens.php" class="btn btn-sm btn-secondary">Ajout d'un produit</a>
+        <a href="listeBiens.php">Ajout d'un produit</a>
     </li>
     <li>
-        <a href="listeBiens.php" class="btn btn-sm btn-secondary">Ajout d'un client</a>
+        <a href="listeBiens.php">Ajout d'un client</a>
     </li>
 </ul>
 
 <ul>
     <li>
-        <a href="listeBiens.php" class="btn btn-sm btn-secondary">Liste des produits</a>
+        <a href="listeBiens.php">Liste des produits</a>
     </li>
     <li>
-        <a href="listeBiens.php" class="btn btn-sm btn-secondary">Liste des clients</a>
+        <a href="listeBiens.php">Liste des clients</a>
     </li>
 </ul>
 
