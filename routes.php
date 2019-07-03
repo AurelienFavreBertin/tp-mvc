df070df14ff39bbae3416dc29a1bf03bed033448
Aurélien FAVRE- BERTIN <aurelien69fb@gmail.com>
2019-07-03 15:42:46 +0200

    routes

--- routes.php
+++ routes.php
@@ -7,6 +7,8 @@ $router = new Router();
 $router->get('', 'PagesController@home' );
 $router->get('clients/add', 'ClientsController@add');
 $router->get('clients/list', 'ClientsController@list');
+$router->get('produits/add', 'ProduitsController@add');
+$router->get('produits/list', 'ProduitsController@list');
 
 // Run it!
 $router->run();
