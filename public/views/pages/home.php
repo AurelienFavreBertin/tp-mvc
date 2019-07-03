f355bf533402d69f13c5240fa51658fafe47dade
Etudiant <info@bdac.eu>
2019-07-03 16:36:33 +0200

    Update home.php

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -1,28 +1,36 @@
 <?php ob_start(); ?>
 
-<h1 class="text-center">Bienvenue !</h1>
+<h1 class="text-center">Bienvenue sur TP-MVC !</h1>
 
 <hr>
+<div style="background-color:lightgrey; border-radius: 10px; padding:20px;">
+    <h3>Ajouter :</h3>
+    <hr>
+    <ol style="list-style-type: none;">
+        <li>
+            <a href="<?= url('clients/add') ?>" class="btn btn-dark">Ajout d'un client</a>
+        </li>
+        <br />
+        <li>
+            <a href="<?= url('produits/add') ?>" class="btn btn-dark">Ajout d'un produit</a>
+        </li>
+    </ol>
+</div>
 
-<ul>
-    <li>
-        <a href="<?= url('clients/add') ?>" class="btn btn-sm btn-secondary">Ajout d'un client</a>
-    </li>
-    <br>
-    <li>
-        <a href="<?= url('produits/add') ?>" class="btn btn-sm btn-secondary">Ajout d'un produit</a>
-    </li>
-</ul>
-<br><br>
-<ul>
-    <li>
-        <a href="<?= url('clients/list') ?>" class="btn btn-sm btn-secondary">Liste des clients</a>
-    </li>
-    <br>
-    <li>
-        <a href="<?= url('produits/list') ?>" class="btn btn-sm btn-secondary">Liste des produits</a>
-    </li>
-</ul>
+<br />
 
+<div style="background-color:lightgrey; border-radius: 10px; padding:20px;">
+    <h3>Visualiser :</h3>
+    <hr>
+    <ol style="list-style-type: none;">
+        <li>
+            <a href="<?= url('clients/list') ?>" class="btn btn-dark">Liste des clients</a>
+        </li>
+        <br />
+        <li>
+            <a href="<?= url('produits/list') ?>" class="btn btn-dark">Liste des produits</a>
+        </li>
+    </ol>
+</div>
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
