32ed13f522be362980bdd317b3617151275ef434
Hovhannisyan <tig.hov90@gmail.com>
2019-07-03 15:57:50 +0200

    fix

--- config/config.php
+++ config/config.php
@@ -13,7 +13,7 @@ const DB_USER = 'root';
 const DB_PWD  = '';
 
 const WEBSITE_TITLE = "";
-const BASE_URL = "http://localhost/cours/tp-mvc/";
+const BASE_URL = "http://localhost/tp-mvc/";
 
 const CLASSES_SOURCES = [
     'src/controller',
