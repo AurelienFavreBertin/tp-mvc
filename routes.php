193bc0a8c6df5879c578afe9c956186186fbfa21
Aurélien FAVRE- BERTIN <aurelien69fb@gmail.com>
2019-07-05 12:15:04 +0200

    delete

--- routes.php
+++ routes.php
@@ -8,6 +8,7 @@ $router->get('', 'PagesController@home' );
 $router->get('clients/add', 'ClientsController@add');
 $router->post('clients/add', 'ClientsController@save');
 $router->get('clients/list', 'ClientsController@list');
+$routes->get('clients/{id}/delete', 'ClientsController@delete');
 $router->get('clients/{id}', 'ClientsController@show');
 
 $router->get('produits/add', 'ProduitsController@add');
