aca62eee4c08c904258b937d92064836f4673895
Etudiant <info@bdac.eu>
2019-07-05 16:10:04 +0200

    Update

--- public/views/produits/list.php
+++ public/views/produits/list.php
@@ -2,29 +2,29 @@
 
 
 <a href="<?= url('') ?>" class="btn btn-sm btn-secondary" style="margin-bottom:30px;">
-    < Retour à la page d'accueil</a>
-    
-    <div class="box">
-        
+    < Retour à la page d'accueil</a> <div class="box">
+
         <table style="margin-top:20px;" class="table">
             <img class="icone left" src="<?= img_url('lister_produits.png') ?>" alt="Lister produits" />
             <h2>Liste des produits</h2>
 
-                <tr>
-                    <th>#id</th>
-                    <th>Nom</th>
-                    <th>Description</th>
-                    <th>Prix</th>
-                    <th>Stock</th>
-                    <th>Photo</th>
-                </tr>
-            
+            <tr>
+                <th>Supprimer</th>
+                <th>Modifier</th>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Prix</th>
+                <th>Stock</th>
+                <th>Photo</th>
+            </tr>
+
 
 
             <?php foreach ($produits as $produit) : ?>
 
                 <tr>
-                    <td><?= $produit['id'] ?></td>
+                    <td><a href="<?= url('produits/' . $produit['id'] . '/delete') ?>"><img class="icone_cli" src="<?= img_url('delete_produit.png') ?>" alt="delete" /></a></td>
+                    <td><a href="<?= url('produits/' . $produit['id'] . '/edit') ?>"><img class="icone_cli" src="<?= img_url('update_produit.png') ?>" alt="update" /></a></td>
                     <td><?= $produit['nom'] ?></td>
                     <td><?= $produit['description'] ?></td>
                     <td><?= $produit['prix'] ?></td>
@@ -36,7 +36,7 @@
 
         </table>
 
-    </div>
+        </div>
 
         <?php $content = ob_get_clean() ?>
         <?php view('template', compact('content')); ?>
